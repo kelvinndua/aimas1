444241bc62bb9b7483cd5c76caf095b181d92e16
kelvinndua <kelvinwaweru00@gmail.com>
2020-09-25 16:13:29 +0300

    Initial commit

---
+++ src/components/pages/Products/Products.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HeroSection from "../../HeroSection";
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from "./Data";
+import Pricing from "../../Pricing";
+function Products() {
+  return (
+    <>
+      <HeroSection {...homeObjTwo} />
+    </>
+  );
+}
+
+export default Products;
---
+++ src/components/pages/Services/Services.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "../../HeroSection";
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from "./Data";
+import Pricing from "../../Pricing";
+function Services() {
+  return (
+    <>
+      <Pricing />
+      <HeroSection {...homeObjTwo} />
+    </>
+  );
+}
+
+export default Services;
